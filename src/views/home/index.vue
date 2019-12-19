4c7399081b20605556486eceea1d02f29697d04b
liubing <1184679882@qq.com>
2019-12-19 21:42:27 +0800

    主要页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>主页</div>
+  <el-container>
+    <el-aside>
+      左侧内容
+    </el-aside>
+    <el-cotainer>
+      <el-header>
+        头部内容
+  </el-header>
+  <el-main>
+    主要区域
+  </el-main>
+    </el-cotainer>
+  </el-container>
+
 </template>
 
 <script>
