a8e73f363e53daebb5314b9833c19e2730801c33
liubing <1184679882@qq.com>
2019-12-19 14:11:56 +0800

    登陆界面完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-      <el-form>
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:65%;" placeholder="验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" style="width:65%;" placeholder="验证码"></el-input>
           <el-button style="float:right;" plain>发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="check">
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%;" type="primary">登录</el-button>
+          <el-button @click="commitLogin" style="width:100%;" type="primary">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -25,7 +25,39 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 检查是否勾选
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '请输入手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }],
+        code: [{ required: true, message: '请输入你的验证码' },
+          { pattern: /^\d{6}$/, message: '验证码格式不正确' }],
+        check: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('请勾选同意'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      // 手动校验：
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          // 说明校验通过；
+          console.log('校验通过，开始调用登录')
+        }
+      })
+    }
+  }
 }
 </script>
 
