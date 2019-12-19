7a1d92b68d3c57155db2711c45a8ecba123b8c0e
liubing <1184679882@qq.com>
2019-12-19 11:27:17 +0800

    登录的布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>登录</div>
+  <div class="login">
+    <el-card class="login-card">
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt="">
+      </div>
+      <el-form>
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input style="width:65%;" placeholder="验证码"></el-input>
+          <el-button style="float:right;" plain>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%;" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,6 +29,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.login {
+    background-image: url(../../assets/img/small1c34660a6917f03b1249f8a1923d36ef1573698572.jpg);
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card {
+      width: 440px;
+      height: 350px;
+      .title {
+        text-align: center;
+        margin-bottom: 30px;
+        img {
+          height: 45px;
+        }
+      }
+    }
 
+}
 </style>
