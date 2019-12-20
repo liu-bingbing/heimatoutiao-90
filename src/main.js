f03c5098dedff0832cd6655d3d7e9d5d2bff07ba
liubing <1184679882@qq.com>
2019-12-20 18:28:47 +0800

    统一注册全局使用方法

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import Component from './components'
 import 'element-ui/lib/theme-chalk/index.css'// 引入样式
 import './style/index.less'
 import axios from 'axios'
 Vue.prototype.$axios = axios// 赋值给全局对象;
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 设置一个常态值;
 Vue.use(ElementUI)
+Vue.use(Component)
 Vue.config.productionTip = false
 
 new Vue({
