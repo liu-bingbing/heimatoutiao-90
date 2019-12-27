b70f355c51cf359b9761f853237638af95f58a9e
liubing <1184679882@qq.com>
2019-12-27 21:09:48 +0800

    注册富文本编辑器并使用

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -10,9 +10,9 @@
             <el-input v-model="formData.title" style="width:60%"></el-input>
         </el-form-item>
         <el-form-item label="内容" prop="content">
-            <el-input v-model="formData.content" type="textarea" :rows="4"></el-input>
+            <quill-editor v-model="formData.content" style="height:300px"></quill-editor>
         </el-form-item>
-        <el-form-item label="封面" prop="cover">
+        <el-form-item label="封面" prop="cover" style="margin-top:120px">
             <el-radio-group v-model="formData.cover.type">
                 <el-radio :label="1">单图</el-radio>
                 <el-radio :label="3">三图</el-radio>
