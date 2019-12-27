b6236243986003fbb7e22ba988fd98c26acd2260
liubing <1184679882@qq.com>
2019-12-27 11:02:29 +0800

    文章发布和保存草稿

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -26,8 +26,8 @@
             </el-select>
         </el-form-item>
         <el-form-item>
-            <el-button @click="publishArticle" type="primary">发布</el-button>
-            <el-button @click="publishArticle">存入草稿</el-button>
+            <el-button @click="publishArticle()" type="primary">发布</el-button>
+            <el-button @click="publishArticle(true)">存入草稿</el-button>
         </el-form-item>
     </el-form>
 </el-card>
@@ -67,10 +67,22 @@ export default {
         this.channels = result.data.channels
       })
     },
-    publishArticle () {
-      this.$$refs.publishForm.validate(isOK => {
+    publishArticle (draft) {
+      this.$refs.publishForm.validate(isOK => {
         if (isOK) {
-          console.log('校验通过')
+          this.$axios({
+            url: '/articles',
+            method: 'post',
+            params: { draft }, // 查询参数
+            data: this.formData // 请求体参数
+          }).then(() => {
+            this.$message({
+              type: 'success',
+              message: '保存成功'
+            })
+            // 跳转到文章列表页
+            this.$router.push('/home/articles')
+          })
         }
       })
     }
