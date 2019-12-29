e246a28b3b3dcf8203d156bbdb79a9f73ccdbd20
liubing <1184679882@qq.com>
2019-12-29 22:23:05 +0800

    账户信息页面搭建

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -3,6 +3,27 @@
     <bread-crumb slot="header">
     <template slot="title">账户信息</template>
     </bread-crumb>
+    <el-form style="margin-left:100px" label-width="100px">
+        <el-form-item label="用户名">
+            <el-input style="width:40%"></el-input>
+        </el-form-item>
+        <el-form-item label="简介">
+            <el-input style="width:40%"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+            <el-input style="width:40%"></el-input>
+            </el-form-item>
+        <el-form-item label="手机号">
+            <el-input disabled style="width:40%"></el-input>
+            </el-form-item>
+        <el-form-item>
+            <el-button type="primary">保存信息</el-button>
+        </el-form-item>
+    </el-form>
+    <!-- 上传组件 -->
+    <el-upload class="head-upload" action="" :show-file-list="false">
+        <img src="../../assets/img/small1c34660a6917f03b1249f8a1923d36ef1573698572.jpg" alt="">
+    </el-upload>
 </el-card>
 </template>
 
@@ -12,6 +33,17 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.head-upload {
+    position: absolute;
+    right: 200px;
+    top: 180px;
+    img {
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+
+    }
+}
 
 </style>
